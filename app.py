e29ee9ab14ee18bb329b64c8adec40b05dc412b4
snewman-aa <snewman.aa@gmail.com>
2024-02-01 17:01:14 -0500

    easyOCR based DocTR

---
+++ app.py
@@ -0,0 +1,99 @@
+"""
+wrapper for DocTR end to end OCR
+"""
+
+import argparse
+import logging
+from typing import Union
+
+# Imports needed for Clams and MMIF.
+# Non-NLP Clams applications will require AnnotationTypes
+
+from clams import ClamsApp, Restifier
+from mmif import Mmif, View, Annotation, Document, AnnotationTypes, DocumentTypes
+from mmif.utils import video_document_helper as vdh
+
+from doctr.models import ocr_predictor
+import torch
+import numpy as np
+
+
+class DoctrWrapper(ClamsApp):
+
+    def __init__(self):
+        super().__init__()
+        gpu = True if torch.cuda.is_available() else False
+        self.reader = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_vgg16_bn', pretrained=True)
+
+    def _appmetadata(self):
+        # see https://sdk.clams.ai/autodoc/clams.app.html#clams.app.ClamsApp._load_appmetadata
+        # Also check out ``metadata.py`` in this directory. 
+        # When using the ``metadata.py`` leave this do-nothing "pass" method here. 
+        pass
+
+    def _annotate(self, mmif: Union[str, dict, Mmif], **parameters) -> Mmif:
+        self.logger.debug("running app")
+        video_doc: Document = mmif.get_documents_by_type(DocumentTypes.VideoDocument)[0]
+        input_view: View = mmif.get_views_for_document(video_doc.properties.id)[0]
+
+        new_view: View = mmif.new_view()
+        self.sign_view(new_view, parameters)
+
+        for timeframe in input_view.get_annotations(AnnotationTypes.TimeFrame):
+            self.logger.debug(timeframe.properties)
+            # get images from time frame
+            self.logger.debug("Sampling 1 frame")
+            image: np.ndarray = vdh.extract_mid_frame(mmif, timeframe, as_PIL=False)
+            self.logger.debug("Extracted image")
+            self.logger.debug("Running OCR")
+            ocrs = []
+            result = self.reader([image])
+            blocks = result.pages[0].blocks
+            for block in blocks:
+                for line in block.lines:
+                    for word in line.words:
+                        ocrs.append((word.geometry, word.value, word.confidence))
+            self.logger.debug(ocrs)
+            timepoint = new_view.new_annotation(AnnotationTypes.TimePoint)
+            timepoint.add_property("timePoint", vdh.get_mid_framenum(mmif, timeframe))
+            point_frame = new_view.new_annotation(AnnotationTypes.Alignment)
+            point_frame.add_property("source", timeframe.id)
+            point_frame.add_property("target", timepoint.id)
+            for ocr in ocrs:
+                for coord, text, score in ocr:
+                    if score > 0.4:
+                        self.logger.debug("Confident OCR: " + text)
+                        text_document = new_view.new_textdocument(text)
+                        bbox_annotation = new_view.new_annotation(AnnotationTypes.BoundingBox)
+                        bbox_annotation.add_property("coordinates", coord)
+                        bbox_annotation.add_property("boxType", "text")
+                        time_bbox = new_view.new_annotation(AnnotationTypes.Alignment)
+                        time_bbox.add_property("source", timepoint.id)
+                        time_bbox.add_property("target", bbox_annotation.id)
+                        bbox_text = new_view.new_annotation(AnnotationTypes.Alignment)
+                        bbox_text.add_property("source", bbox_annotation.id)
+                        bbox_text.add_property("target", text_document.id)
+
+        return mmif
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", action="store", default="5000", help="set port to listen" )
+    parser.add_argument("--production", action="store_true", help="run gunicorn server")
+    # add more arguments as needed
+    # parser.add_argument(more_arg...)
+
+    parsed_args = parser.parse_args()
+
+    # create the app instance
+    app = DoctrWrapper()
+
+    http_app = Restifier(app, port=int(parsed_args.port))
+    # for running the application in production mode
+    if parsed_args.production:
+        http_app.serve_production()
+    # development mode
+    else:
+        app.logger.setLevel(logging.DEBUG)
+        http_app.run()
---
+++ metadata.py
@@ -0,0 +1,52 @@
+"""
+The purpose of this file is to define the metadata of the app with minimal imports. 
+
+DO NOT CHANGE the name of the file
+"""
+
+from mmif import DocumentTypes, AnnotationTypes
+
+from clams.app import ClamsApp
+from clams.appmetadata import AppMetadata
+
+
+# DO NOT CHANGE the function name 
+def appmetadata() -> AppMetadata:
+    """
+    Function to set app-metadata values and return it as an ``AppMetadata`` obj.
+    Read these documentations before changing the code below
+    - https://sdk.clams.ai/appmetadata.html metadata specification. 
+    - https://sdk.clams.ai/autodoc/clams.appmetadata.html python API
+    
+    :return: AppMetadata object holding all necessary information.
+    """
+    
+    # first set up some basic information
+    metadata = AppMetadata(
+        name="DocTR Wrapper",
+        description="End to end OCR for extracting text from timeframes",
+        app_license="MIT",
+        identifier="doctr-wrapper",
+        url="https://github.com/clamsproject/app-easyocr-wrapper",
+        analyzer_version='0.7.0',
+        analyzer_license="Apache 2.0",
+    )
+    metadata.add_input(DocumentTypes.VideoDocument)
+    metadata.add_output(DocumentTypes.TextDocument)
+    metadata.add_output(AnnotationTypes.Alignment)
+    metadata.add_output(AnnotationTypes.BoundingBox)
+    metadata.add_output(AnnotationTypes.TimePoint)
+
+    metadata.add_parameter(name='sampleFrames', description='Number of frames to sample from timeframe',
+                           type='integer', default='1')
+    
+    return metadata
+
+
+# DO NOT CHANGE the main block
+if __name__ == '__main__':
+    import sys
+    metadata = appmetadata()
+    for param in ClamsApp.universal_parameters:
+        metadata.add_parameter(**param)
+    sys.stdout.write(metadata.jsonify(pretty=True))
