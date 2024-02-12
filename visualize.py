75de5dd51ed88fe52b759529528073d2730fff9d
snewman-aa <snewman.aa@gmail.com>
2024-02-12 17:37:46 -0500

    cleanup

--- visualize.py
+++ visualize.py
@@ -13,25 +13,24 @@ import cv2 as cv
 
 class OCR:
     def __init__(self, image):
-        self.reader = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_mobilenet_v3_large', pretrained=True, detect_orientation=True, paragraph_break=0.015, assume_straight_pages=True).to(torch.device("cuda:0"))
+        self.reader = ocr_predictor(det_arch='db_resnet50', reco_arch='crnn_mobilenet_v3_large',
+                                    pretrained=True, detect_orientation=True, paragraph_break=0.015,
+                                    assume_straight_pages=True).to(torch.device("cuda:0"))
         self.image = image
         self.results = self.reader(image)
 
 
 def draw(results: doctr.io.Document, image):
-    print('drawing')
     annotated_img = image.copy()
     height, width = results.pages[0].dimensions
     print(results.pages[0].blocks)
     for block in results.pages[0].blocks:
         block_bb = ((int(block.geometry[0][0] * width), int(block.geometry[0][1] * height)),
                     (int(block.geometry[1][0] * width), int(block.geometry[1][1] * height)))
-        print(f'drawing block with {block_bb}')
         annotated_img = cv.rectangle(annotated_img, block_bb[0], block_bb[1], (255, 0, 0), 3)
         for line in block.lines:
             line_bb = ((int(line.geometry[0][0] * width ), int(line.geometry[0][1] * height)),
                         (int(line.geometry[1][0] * width), int(line.geometry[1][1] * height)))
-            print(f'drawing line with {line_bb}')
             annotated_img = cv.rectangle(annotated_img, line_bb[0], line_bb[1], (0, 255, 0), 2)
     return annotated_img
 
